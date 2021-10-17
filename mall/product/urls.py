08c3cfe94ab392a3748e60d0ce0a49a7a477678e
김유현 <sustkadl12@gmail.com>
2021-10-17 11:29:28 +0000

    2021-10-17 updates

--- mall/product/urls.py
+++ mall/product/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from product.views import ProductList
 from product.views import ProductCreate
+from product.views import ProductDetail
 
 urlpatterns = [
     path('', ProductList.as_view()),
     path('create/', ProductCreate.as_view()),
+    # primary key
+    path('<int:pk>/', ProductDetail.as_view())
 ]
--- mall/product/views.py
+++ mall/product/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic import DetailView
 from django.views.generic.edit import FormView
 from .models import Product
 from .forms import RegisterForm
@@ -13,3 +14,10 @@ class ProductCreate(FormView):
     template_name = 'register_product.html'
     form_class = RegisterForm 
     success_url = '/product/'
+    
+class ProductDetail(DetailView):
+    template_name = 'product_detail.html'
+    queryset = Product.objects.all()
+    # 아래 내용을 설정하면 for item in product 처럼
+    # 사용할 수 있음 -> humanize
+    context_object_name = 'product'
